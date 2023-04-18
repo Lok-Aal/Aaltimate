b6f014fd1fae8ba9e10143f61b5108c78190d01a
Lucs777 <i20024@hb.dhbw-stuttgart.de>
2023-04-18 15:54:51 +0200

    first tests (telephone not included rn)

--- backend/test/number-parser.spec.ts
+++ backend/test/number-parser.spec.ts
@@ -1,11 +1,69 @@
 import { parsePhoneNumber } from "../src/utils/number-parser";
 import { PhoneNumber } from "../src/utils/types";
 
+//das sollte alles gehen
+/*
++49 0201 123456
++44 0201123456
+0033 0201/123456
+0049201123456
+(0)201 1234 56
++49 (941) 790-4780
+015115011900
++91 09870987 899
+[+49] (0)89-800/849-50
++49 (8024) [990-477]
+*/
 
 describe('parsePhoneNumber', () => {
-    it('parses Number into slices ', () => {
+    it('+49 0201 123456', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"49",durchwahl:"",hauptwahl:"123456",ortsvorwahl:"0201"}
+      expect(parsePhoneNumber("+49 0201 123456"))
+          .toEqual(telephone);
+    });
+    it('+44 0201123456', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"44",durchwahl:"",hauptwahl:"123456",ortsvorwahl:"0201"}
+      expect(parsePhoneNumber("+44 0201123456"))
+          .toEqual(telephone);
+    });
+    it('0033 0201/123456', () => {
         let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
-      expect(parsePhoneNumber("3429238432"))
+      expect(parsePhoneNumber("0033 0201/123456"))
+          .toEqual(telephone);
+    });
+    it('0049201123456', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("0049201123456"))
+          .toEqual(telephone);
+    });
+    it('(0)201 1234 56', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("(0)201 1234 56"))
+          .toEqual(telephone);
+    });
+    it('+49 (941) 790-4780', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("+49 (941) 790-4780"))
+          .toEqual(telephone);
+    });
+    it('015115011900', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("015115011900"))
+          .toEqual(telephone);
+    });
+    it('+91 09870987 899', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("+91 09870987 899"))
+          .toEqual(telephone);
+    });
+    it('[+49] (0)89-800/849-50', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("[+49] (0)89-800/849-50"))
+          .toEqual(telephone);
+    });
+    it('+49 (8024) [990-477]', () => {
+        let telephone:PhoneNumber = {landesvorwahl:"34",durchwahl:"4343",hauptwahl:"3434",ortsvorwahl:"3443"}
+      expect(parsePhoneNumber("+49 (8024) [990-477]"))
           .toEqual(telephone);
     });
   });
