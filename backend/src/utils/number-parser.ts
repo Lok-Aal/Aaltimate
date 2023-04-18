b527488ddafe498ffc718e078395dcd44f45ddfa
Fabian <fabian.klimpel2002@gmail.com>
2023-04-18 16:16:56 +0200

    parsePhoneNumber countryMatch

--- backend/src/utils/number-parser.ts
+++ backend/src/utils/number-parser.ts
@@ -1,13 +1,37 @@
 import { PhoneNumber } from "./types";
 
-export function parsePhoneNumber(number: string): PhoneNumber {
+export function parsePhoneNumber(number: string): PhoneNumber | null {
+    
+    let country_match = countryMatch(number);
+
+    if (!country_match) {
+        return null;
+    }
+
+    let [country_code, number_rest] = country_match;
+
+    
 
     let telephoneNumber: PhoneNumber = {
-        landesvorwahl: number,
-        ortsvorwahl: number,
-        hauptwahl: number,
-        durchwahl: number
+        landesvorwahl: country_code,
+        ortsvorwahl: "",
+        hauptwahl: number_rest,
+        durchwahl: ""
     }
 
     return telephoneNumber;
+}
+
+function countryMatch(number: string): [string, string] | null {
+    const COUNTRY_PATTERN = /^(\+\d{1,2}|0)([ 0-9\/()\[\]-]*)$/;
+    
+    let matches = number.match(COUNTRY_PATTERN);
+
+    if (!matches) {
+        return null;
+    }
+
+    let [, country_code, number_rest] = matches;
+    
+    return [country_code, number_rest.trim()];
 }
